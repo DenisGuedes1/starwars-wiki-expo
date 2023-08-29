95121d5cd8107d736f3d2e47b1c98eeaf61f6cfc
DenisGuedes1 <denisguedesmoraes@gmail.com>
2023-08-29 14:05:21 -0300

    finish screen one

---
+++ src/components/atoms/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {LogoImage}from'./styles'
+import LogoEmpire from"../../../../assets/LogoEmpire.png"
+
+export const Logo = ()=>{
+    return(
+        <LogoImage source={LogoEmpire} />
+    )
+}
+
